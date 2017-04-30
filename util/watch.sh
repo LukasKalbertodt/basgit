79ebd81515ce67f987a9b4db7e8b1efaec86c3aa
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2017-04-30 13:30:51 +0200

    Tweak watch-script
    Disable incremental compilation as it results in annoying warnings

--- util/watch.sh
+++ util/watch.sh
@@ -2,6 +2,6 @@
 
 MY_DIR=$(dirname $0)
 
-export CARGO_INCREMENTAL=1
+# export CARGO_INCREMENTAL=1
 watchexec -f '*.less' "$MY_DIR/do-less.sh" &
-watchexec --restart -i '*.less' -i '*.svg' "clr && git status -s && echo '------------' && cargo build && RUST_BACKTRACE=1 ./target/debug/basgit"
+watchexec --restart -i '*.less' -i '*.svg' -i '*.sql' "clr && git status -s && echo '------------' && cargo build && RUST_BACKTRACE=1 ./target/debug/basgit"
