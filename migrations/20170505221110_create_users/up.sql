1225c3c001c9a81122f8054df447e61b1e2ab263
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2017-05-07 22:34:34 +0200

    Fix comment position

--- migrations/20170505221110_create_users/up.sql
+++ migrations/20170505221110_create_users/up.sql
@@ -14,13 +14,12 @@ create unique index users_unique_lower_username_idx on users (lower(username));
 
 
 create table user_emails (
-    email text primary key check (octet_length(email) <= 254),
-    user_id bigint not null references users(id) on delete cascade on update cascade
-
     -- Valid emails can't be longer than 254 bytes. We can at least restrict
     -- the number of characters here.
     --
     -- [1]: http://stackoverflow.com/a/574698/2408867
+    email text primary key check (octet_length(email) <= 254),
+    user_id bigint not null references users(id) on delete cascade on update cascade
 );
 
 create index user_emails_user_id_idx on user_emails (user_id);
